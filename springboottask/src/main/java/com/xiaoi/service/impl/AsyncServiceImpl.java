2216473230ffe85257f372bcc8ad2ca10f20d1f4
kevin.zhu <abc15855364086>
2020-01-09 11:23:29 +0800

    feat: add slf log

--- springboottask/src/main/java/com/xiaoi/service/impl/AsyncServiceImpl.java
+++ springboottask/src/main/java/com/xiaoi/service/impl/AsyncServiceImpl.java
@@ -1,13 +1,16 @@
 package com.xiaoi.service.impl;
 
 import com.xiaoi.service.AsyncService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import sun.rmi.runtime.Log;
 
 /**
  * @author kevin.zhu
  * @date 2019/3/31 10:51
  */
+@Slf4j
 @Service
 public class AsyncServiceImpl implements AsyncService {
 
@@ -15,6 +18,7 @@ public class AsyncServiceImpl implements AsyncService {
     @Async
     public void asyncTask() {
         try {
+            log.info("睡眠开始");
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
